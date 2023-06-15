70ed339bf7b32314cc9381884fd0d190fa938c83
blakepf <110067366+blakepf@users.noreply.github.com>
2023-06-15 10:54:28 -0400

    login controller, request, and create request built.

--- Application/src/main/java/com/kenzie/appserver/service/LoginService.java
+++ Application/src/main/java/com/kenzie/appserver/service/LoginService.java
@@ -11,70 +11,70 @@ import java.util.Optional;
 @Service
 public class LoginService {
 
-        private final LoginRepository loginRepository;
+    private final LoginRepository loginRepository;
 
-        @Autowired
-        public LoginService(LoginRepository loginRepository) {
-                this.loginRepository = loginRepository;
+    @Autowired
+    public LoginService(LoginRepository loginRepository) {
+        this.loginRepository = loginRepository;
+    }
+
+    public boolean createLogin(String email, String password) {
+        Optional<LoginRecord> record = loginRepository.findById(email);
+        if (record.isPresent()) {
+            return false;
+        } else {
+            LoginRecord loginRecord = new LoginRecord(email, password);
+            loginRepository.save(loginRecord);
+            return true;
         }
 
-        public boolean createLogin(String email, String password){
-                Optional<LoginRecord> record = loginRepository.findById(email);
-                if (record.isPresent()){
-                        return false;
-                }else {
-                        LoginRecord loginRecord = new LoginRecord(email, password);
-                        loginRepository.save(loginRecord);
-                        return true;
-                }
+    }
 
+    public Login login(String email, String password) {
+        Optional<LoginRecord> record = loginRepository.findById(email);
+        if (record.isPresent()) {
+            LoginRecord loginRecord = record.get();
+            if (loginRecord.getPassword().equals(password)) {
+                return new Login(email, password);
+            } else {
+                return null;
+            }
+        } else {
+            return null;
         }
+    }
 
-        public Login login(String email, String password){
-                Optional<LoginRecord> record = loginRepository.findById(email);
-                if (record.isPresent()){
-                        LoginRecord loginRecord = record.get();
-                        if (loginRecord.getPassword().equals(password)){
-                                return new Login(email, password);
-                        }else {
-                                return null;
-                        }
-                }else {
-                        return null;
-                }
+    public boolean deleteLoginByEmail(String email) {
+        Optional<LoginRecord> record = loginRepository.findById(email);
+        if (record.isPresent()) {
+            loginRepository.delete(record.get());
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        public  boolean deleteLoginByEmail(String email){
-                Optional<LoginRecord> record = loginRepository.findById(email);
-                if (record.isPresent()){
-                        loginRepository.delete(record.get());
-                        return true;
-                }else {
-                        return false;
-                }
+    public boolean updatePasswordByEmail(String email, String password) {
+        Optional<LoginRecord> record = loginRepository.findById(email);
+        if (record.isPresent()) {
+            LoginRecord loginRecord = record.get();
+            loginRecord.setPassword(password);
+            loginRepository.save(loginRecord);
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        public boolean updatePasswordByEmail(String email, String password){
-                Optional<LoginRecord> record = loginRepository.findById(email);
-                if (record.isPresent()) {
-                        LoginRecord loginRecord = record.get();
-                        loginRecord.setPassword(password);
-                        loginRepository.save(loginRecord);
-                        return true;
-                }else {
-                        return false;
-                }
+    public boolean updateEmailByEmail(String email, String updatedEmail) {
+        Optional<LoginRecord> record = loginRepository.findById(email);
+        if (record.isPresent()) {
+            LoginRecord loginRecord = record.get();
+            loginRecord.setEmail(updatedEmail);
+            loginRepository.save(loginRecord);
+            return true;
+        } else {
+            return false;
         }
-
-        public boolean updateEmailByEmail(String email, String updatedEmail){
-                Optional<LoginRecord> record = loginRepository.findById(email);
-                if (record.isPresent()) {
-                        LoginRecord loginRecord = record.get();
-                        loginRecord.setEmail(updatedEmail);
-                        loginRepository.save(loginRecord);
-                        return true;
-                }else {
-                        return false;
-                }
-        }
+    }
 }
