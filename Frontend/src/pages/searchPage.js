0a800a84c508c2196803fa929a53a6c716fab9aa
michaelalfonsocode <110067139+michaelalfonsocode@users.noreply.github.com>
2023-06-20 17:00:04 -0400

    update

--- Frontend/src/pages/searchPage.js
+++ Frontend/src/pages/searchPage.js
@@ -1,6 +1,7 @@
 import BaseClass from "../util/baseClass";
 import DataStore from "../util/DataStore";
 import HomePageClient from "../api/homePageClient";
+import AnimeClient from "../api/animeClient";
 
 class SearchPage extends BaseClass{
 
@@ -11,18 +12,39 @@ class SearchPage extends BaseClass{
     }
 
     async mount() {
-
-        this.dataStore.addChangeListener(this.renderSearchPage)
-        this.client = new HomePageClient();
-        this.renderSearchPage();
+        document.getElementById("search-form").addEventListener("submit", this.renderSearchPage);
+        this.client = new AnimeClient();
     }
 
-    async renderSearchPage(){
+    async renderSearchPage(event){
+        event.preventDefault()
 
         let workArea = document.getElementById("work-area");
+        let searchValue = document.getElementById("search-bar").value;
+        let searchValueIntoString = `${searchValue}`;
 
+        console.log(searchValue);
 
+        const newResponse = await this.client.getAnimeBySearch(searchValueIntoString, this.errorHandler);
 
+        let items = "";
+
+        console.log(newResponse[0]);
+        for(let i = 0; i < newResponse.length; i++){
+            items +=`
+            <div class="poster">
+            <a href="animepage.html"><img class = "poster-click" alt=${newResponse[i].id} src=${newResponse[i].coverImage.large} width="160px" height="90%"></a>    
+            </div>
+         `;
+        }
+
+        workArea.innerHTML = items;
+
+        document.querySelectorAll(".poster-click").forEach(poster =>{
+            poster.addEventListener("click", async function(){
+                sessionStorage.setItem("animeCode", poster.alt)
+            });
+        });
     }
 
 }
