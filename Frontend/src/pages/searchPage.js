b4639e53c9ae09122e3dfa553ba1a002536f076c
melijohnson19 <melissa.johnson@mykenzie.snhu.edu>
2023-06-30 13:29:34 -0400

    SearchPage animations

--- Frontend/src/pages/searchPage.js
+++ Frontend/src/pages/searchPage.js
@@ -6,14 +6,19 @@ import AnimeClient from "../api/animeClient";
 class SearchPage extends BaseClass {
     constructor() {
         super();
-        this.bindClassMethods(['renderSearchPage', 'searchByGenre'], this);
+        this.bindClassMethods(['renderSearchPage', 'searchByGenre', 'animateKero'], this);
         this.dataStore = new DataStore();
         this.genreButtons = Array.from(document.querySelectorAll('.button-86'));
+        this.keroElement = document.getElementById('kero');
+        this.animateKero = this.animateKero.bind(this);
+        this.animateKero();
+
     }
 
     async mount() {
         document.getElementById("search-form").addEventListener("submit", this.renderSearchPage);
         this.genreButtons.forEach(button => button.addEventListener("click", this.searchByGenre));
+        this.keroElement.addEventListener("animationstart", this.animateKero);
         this.client = new AnimeClient();
     }
 
@@ -79,6 +84,25 @@ class SearchPage extends BaseClass {
                 sessionStorage.setItem("animeCode", poster.alt);
             });
         });
+    }
+
+    animateKero() {
+        const animationDuration = 7000;
+        const startX = 0; // Initial X coordinate
+        const startY = 0; // Initial Y coordinate
+        const endX = 1500; // Final X coordinate
+        const endY = 40; // Final Y coordinate
+
+        // Apply the animation using CSS transitions
+        this.keroElement.style.transition = `transform ${animationDuration}ms linear`;
+        this.keroElement.style.transform = `translate(${endX}px, ${endY}px)`;
+
+        // Reset the Kero's position after the animation ends
+        setTimeout(() => {
+            this.keroElement.style.transition = '';
+            this.keroElement.style.transform = `translate(${startX}px, ${startY}px)`;
+            this.animateKero(); // Repeat the animation
+        }, animationDuration);
     }
 }
 
