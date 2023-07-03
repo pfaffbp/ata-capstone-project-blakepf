aea07e06f309ef18ed480e32c255466e5b3f3e56
blakepf <110067366+blakepf@users.noreply.github.com>
2023-07-03 14:00:20 -0400

    fixing webpack config

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -3,13 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve("buffer"),
+      stream: require.resolve("stream-browserify")
+    },
+  },
   optimization: {
     usedExports: true
   },
   entry: {
     examplePage: path.resolve(__dirname, 'src', 'pages', 'examplePage.js'),
+    loginPage: path.resolve(__dirname, 'src', 'pages', 'loginPage.js'),
+    signupPage: path.resolve(__dirname, 'src', 'pages', 'signupPage.js'),
+    updatePasswordPage: path.resolve(__dirname, 'src', 'pages', 'updatePasswordPage.js'),
+    updateLoginPage: path.resolve(__dirname, 'src', 'pages', 'updateLoginPage.js'),
+    homePage: path.resolve(__dirname, 'src', 'pages', 'homePage.js'),
+    animePage: path.resolve(__dirname, 'src', 'pages', 'animePage.js')
   },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
@@ -18,10 +33,14 @@ module.exports = {
     https: false,
     port: 8080,
     open: true,
+
+    /*    openPage: 'http://localhost:8080',
+        disableHostCheck: true,*/
     proxy: [
       {
         context: [
-          '/example',
+          '/',
+
         ],
         target: 'http://localhost:5001'
       }
@@ -33,6 +52,38 @@ module.exports = {
       filename: 'index.html',
       inject: false
     }),
+
+    new HtmlWebpackPlugin({
+      template: './src/signup.html',
+      filename: 'signup.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/login.html',
+      filename: 'login.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/updatePassword.html',
+      filename: 'updatePassword.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/updateLogin.html',
+      filename: 'updateLogin.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/homepage.html',
+      filename: 'homepage.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/animepage.html',
+      filename: 'animepage.html',
+      inject: false
+    }),
+
     new CopyPlugin({
       patterns: [
         {
@@ -40,8 +91,11 @@ module.exports = {
           to: path.resolve("dist/css")
         }
       ]
+
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+
   ]
+
 }
 
